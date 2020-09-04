1ec4ac14b6d962b11f50d14d78a0d0a497cac26c
jbrule <jbrulz@gmail.com>
2020-09-04 16:57:44 -0500

    Update TrackingCodeCustomizer.php

--- TrackingCodeCustomizer.php
+++ TrackingCodeCustomizer.php
@@ -40,20 +40,11 @@ class TrackingCodeCustomizer extends \Piwik\Plugin
             'Piwik.getJavascriptCode' => 'applyTrackingCodeCustomizations'
         );
     
-    //2.15 includes a new function for registering hooks. 2.15 wi
     public function registerEvents()
     {
         return self::$hooks;
     }
-    
-    //Pre 2.15 hook registration. Will be removed in Piwik 3. Kept for backwards compatibility with 2.12.
-    //Pre ver3 will still call this in addition to registerEvents.
-    //From reviewing the Piwik source this shouldn't be an issue as the hooks are not additive and this call will just overwrite the registerEvents call.
-    public function getListHooksRegistered()
-    {
-        return self::$hooks;
-    }
-    
+        
     /*
     * @param array &$sysparams 
     *        @key int idSite
